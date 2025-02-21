1585f83460df845178a4532ae3d8060e6f140035
Harshil Vanparia <harshil25803@gmail.com>
2025-02-21 22:06:57 +0530

    Complete the About Us page.

---
+++ Controllers/OtherPagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheRoyalTourism.Controllers
+{
+    public class OtherPagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+    }
+}
