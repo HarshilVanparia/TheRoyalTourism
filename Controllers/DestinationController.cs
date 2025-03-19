5c1b06849bf9274e4bb36ed3100a58ddff969ac0
Harshil Vanparia <harshil25803@gmail.com>
2025-03-19 19:26:54 +0530

    Complete Contact Us Page, Domestic Destination Page and International Destination Page.

---
+++ Controllers/DestinationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheRoyalTourism.Controllers
+{
+    public class DestinationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Domestic()
+        {
+            return View();
+        }
+
+        public IActionResult International()
+        {
+            return View();
+        }
+    }
+}
