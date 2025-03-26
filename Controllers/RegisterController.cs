faf2863d634e46f420895e9aa8d6c3c597f52e74
Harshil Vanparia <harshil25803@gmail.com>
2025-03-26 19:42:36 +0530

    Completed Login & Register Page.

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheRoyalTourism.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+    }
+}
